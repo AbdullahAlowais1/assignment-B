5e101e41b16a96019eeaec6daa13208e82b54b0b
AbdullahAlowais1 <116824637+AbdullahAlowais1@users.noreply.github.com>
2022-11-13 14:34:48 +0400

    Add files via upload

---
+++ JavaScript-Assignment-B.js
@@ -0,0 +1,82 @@
+/*
+     JavaScript Assignment B
+     ASSIGNMENT NOTES:
+     Do not change the code below. 
+     Add your code in the 'Answers Section'.
+*/
+
+var house = {
+    areas: {
+        livingRoom: {
+            items: ['tv','sofa']
+        },
+        bedroomOne: {
+            items: ['bed','washing machine'],
+            windows: 3
+        },
+        bedroomTwo:{
+            items: ['bed','bed','desk'],
+            windows: 4
+        },
+        kitchen:{
+            items: ['fridge','broken chair','microwave']
+        }
+    },
+    garden: [true, 'Red Rose'],
+    garage: {
+        car: {
+            color: 'red',
+            wheels: 4,
+            honk: function(){ alert("Beep") }
+        },
+    }
+}
+
+
+
+/******** Answers Section ********/
+
+/**
+ * PART I
+ */
+
+// (1) Add a dining table to the living room.
+house.areas.livingRoom.items.push('dining table')
+
+// (2) Add a stove to the kitchen.
+house.areas.kitchen.items.push('stove')
+
+// (3) Remove the washing machine from bedroomOne.
+house.areas.bedroomOne.items.pop()
+
+// (4) Change the color of the car to blue.
+house.garage.car.color = 'blue'
+
+// (5) Add a another car to the garage with a honk function.
+
+
+// (6) Make the new car honk.
+
+// (7) If the house has a garden, console.log the name of the flower.
+if(house.garden){
+    console.log(house.garden[1])
+}
+
+
+
+
+/**
+ * PART II
+ */
+
+// (8) Change the 'broken chair' in the kitchen to 'new chair'. Hint: Use the .replace() function
+
+
+// (9) Find total number of areas in the house. Hint: Use the Object.keys() function.
+house.areas.length
+
+// (10) Find the total number of beds in all rooms. Hint: Use the Object.values() function.
+house.areas.bedroomOne.bedroomTwo.value('bed')
+
+
+console.log(house)
